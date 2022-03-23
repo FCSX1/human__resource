da78a05c83bb3c5549f5cdeb5d67bd7cf45de70b
NFC <1972846283@qq.com>
2022-03-24 00:36:47 +0800

    封装了一个登录接口

--- src/api/user.js
+++ src/api/user.js
@@ -1,7 +1,13 @@
 import request from '@/utils/request'
 
+/* 登录接口的封装 */
 export function login(data) {
-  return request({})
+  // 返回一个promise对象
+  return request({
+    url: '/sys/login',
+    method: 'post',
+    data // 请求体参数  ES6写法变量名和参数名一致时可以简写
+  })
 }
 
 export function getInfo(token) {

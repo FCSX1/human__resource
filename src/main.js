44cc6041573c5d320982828cee1cee6c2334840f
NFC <1972846283@qq.com>
2022-03-25 16:48:37 +0800

    在main.js完成自定义指定全局注册

--- src/main.js
+++ src/main.js
@@ -12,27 +12,17 @@ import App from './App'
 import store from './store'
 import router from './router'
 
+import * as directives from '@/directives'
 import '@/icons' // icon
 import '@/permission' // permission control
-
-/**
- * If you don't want to use mock-server
- * you want to use MockJs for mock api
- * you can execute: mockXHR()
- *
- * Currently MockJs will be used in the production environment,
- * please remove it before going online ! ! !
- */
-// if (process.env.NODE_ENV === 'production') {
-//   const { mockXHR } = require('../mock')
-//   mockXHR()
-// }
-
 // set ElementUI lang to EN
 Vue.use(ElementUI, { locale })
 // 如果想要中文版 element-ui，按如下方式声明
 // Vue.use(ElementUI)
 
+Object.keys(directives).forEach(key => {
+  Vue.directive(key, directives[key]) // 注册自定义指令
+})
 Vue.config.productionTip = false
 
 new Vue({

f11d228ae16626d74e6aaea722ca7157a9a094d4
NFC <1972846283@qq.com>
2022-03-28 21:22:37 +0800

    完成新增表单的基本校验条件

--- src/views/departments/components/add-dept.vue
+++ src/views/departments/components/add-dept.vue
@@ -3,26 +3,26 @@
   <el-dialog title="新增部门" :visible="showDialog">
     <!-- 表单数据 label-width设置标题的宽度 -->
     <el-form label-width="120px">
-      <el-form-item label="部门名称">
-        <el-input style="width:80%" placeholder="1-50个字符">
+      <el-form-item label="部门名称" prop="name">
+        <el-input v-model="formData.name" style="width:80%" placeholder="1-50个字符">
           <!--  -->
         </el-input>
       </el-form-item>
 
-      <el-form-item label="部门编码">
-        <el-input style="width:80%" placeholder="1-50个字符">
+      <el-form-item label="部门编码" prop="code">
+        <el-input v-model="formData.code" style="width:80%" placeholder="1-50个字符">
           <!--  -->
         </el-input>
       </el-form-item>
 
-      <el-form-item label="部门负责人">
-        <el-select style="width:80%" placeholder="请选择">
+      <el-form-item label="部门负责人" prop="manager">
+        <el-select v-model="formData.manager" style="width:80%" placeholder="请选择">
           <!--  -->
         </el-select>
       </el-form-item>
 
-      <el-form-item label="部门介绍">
-        <el-input style="width:80%" placeholder="1-300个字符" type="textarea" :rows="3" />
+      <el-form-item label="部门介绍" prop="introduce">
+        <el-input v-model="formData.introduce" style="width:80%" placeholder="1-300个字符" type="textarea" :rows="3" />
       </el-form-item>
     </el-form>
     <!-- 确认和消息 -->
@@ -41,6 +41,18 @@ export default {
     showDialog: {
       type: Boolean,
       default: false
+    }
+  },
+  data() {
+    return {
+      // 定义一个表单数据
+      formData: {
+        name: '',
+        code: '',
+        manager: '',
+        introduce: ''
+      },
+      rules: {} // 校验规则 {key:数组}
     }
   }
 }

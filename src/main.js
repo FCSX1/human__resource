d3415b22f68979e2effbfb4f9a5b0a39e3c05d36
NFC <1972846283@qq.com>
2022-03-30 21:25:54 +0800

    组件统一注册

--- src/main.js
+++ src/main.js
@@ -12,6 +12,7 @@ import App from './App'
 import store from './store'
 import router from './router'
 
+import Components from '@/components'
 import * as directives from '@/directives'
 import '@/icons' // icon
 import '@/permission' // permission control
@@ -23,6 +24,8 @@ Vue.use(ElementUI, { locale })
 Object.keys(directives).forEach(key => {
   Vue.directive(key, directives[key]) // 注册自定义指令
 })
+// 注册自定义组件
+Vue.use(Components)
 Vue.config.productionTip = false
 
 new Vue({

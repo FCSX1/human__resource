c782676945c48cd7ee8af0d29336d05dcc426b34
NFC <1972846283@qq.com>
2022-03-31 14:12:45 +0800

    删除员工功能

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -48,13 +48,19 @@
         </el-table-column>
 
         <el-table-column label="操作" sortable="" fixed="right" width="280px">
-          <template>
+          <!-- <template slot-scope="{ row }"> -->
+          <template v-slot="{row}">
             <el-button type="text" size="small">查看</el-button>
             <el-button type="text" size="small">转正</el-button>
             <el-button type="text" size="small">调岗</el-button>
             <el-button type="text" size="small">离职</el-button>
             <el-button type="text" size="small">角色</el-button>
-            <el-button type="text" size="small">删除</el-button>
+
+            <el-button
+              type="text"
+              size="small"
+              @click="delEmployee(row.id)"
+            >删除</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -78,7 +84,7 @@
 </template>
 
 <script>
-import { getEmployeeList } from '@/api/employees'
+import { getEmployeeList, delEmployee } from '@/api/employees'
 import EmployeeEnum from '@/api/constant/employees' // 引入员工的枚举对象
 export default {
   data() {
@@ -113,6 +119,17 @@ export default {
       // 要去找 1所对应的值
       const obj = EmployeeEnum.hireType.find(item => item.id === cellValue)
       return obj ? obj.value : '未知'
+    },
+    async delEmployee(id) {
+      try {
+        await this.$confirm('确定删除该员工吗？')
+        // 点击了确定
+        await delEmployee(id)
+        this.$message.success('删除员工成功')
+        this.getEmployeeList() // 重新拉去数据
+      } catch (error) {
+        console.log(error)
+      }
     }
   }
 }

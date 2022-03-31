6a1f33167f33cc30aecc94443f25a8c1066234af
NFC <1972846283@qq.com>
2022-03-31 10:36:03 +0800

    员工列表数据请求和分页加载

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -5,7 +5,7 @@
         <!-- 左侧显示总记录数 -->
         <!-- <span slot="before">共16条记录</span> -->
         <template v-slot:before>
-          <span>共16条记录</span>
+          <span>共{{ page.total }}条记录</span>
         </template>
         <!-- 右侧显示按钮  excel导入 excel导出 新增员工 -->
         <template v-slot:after>
@@ -15,14 +15,14 @@
         </template>
       </page-tools>
       <!-- 表格组件 -->
-      <el-table>
-        <el-table-column label="序号" sortable="" />
-        <el-table-column label="姓名" sortable="" />
-        <el-table-column label="工号" sortable="" />
-        <el-table-column label="聘用形式" sortable="" />
-        <el-table-column label="部门" sortable="" />
-        <el-table-column label="入职时间" sortable="" />
-        <el-table-column label="账户状态" sortable="" />
+      <el-table v-loading="loading" :data="list">
+        <el-table-column type="index" label="序号" sortable="" />
+        <el-table-column label="姓名" prop="username" sortable="" />
+        <el-table-column label="工号" prop="workNumber" sortable="" />
+        <el-table-column label="聘用形式" prop="formOfEmployment" sortable="" />
+        <el-table-column label="部门" prop="departmentName" sortable="" />
+        <el-table-column label="入职时间" prop="timeOfEntr" sortable="" />
+        <el-table-column label="账户状态" prop="enableState" sortable="" />
         <el-table-column label="操作" sortable="" fixed="right" width="280px">
           <template>
             <el-button type="text" size="small">查看</el-button>
@@ -41,15 +41,48 @@
         align="middle"
         style="height: 60px"
       >
-        <el-pagination layout="prev,pager,next" />
+        <el-pagination
+          :current-page="page.page"
+          :page-size="page.size"
+          :total="page.total"
+          layout="prev,pager,next"
+          @current-change="changePage"
+        />
       </el-row>
     </div>
   </div>
 </template>
 
 <script>
+import { getEmployeeList } from '@/api/employees'
 export default {
-
+  data() {
+    return {
+      list: [], // 接收数组
+      page: {
+        page: 1,
+        size: 10,
+        total: 0 // 总数
+      },
+      loading: false // 显示遮罩层
+    }
+  },
+  created() {
+    this.getEmployeeList()
+  },
+  methods: {
+    async getEmployeeList() {
+      this.loading = true
+      const { total, rows } = await getEmployeeList(this.page)
+      this.page.total = total
+      this.list = rows
+      this.loading = false
+    },
+    changePage(newPage) {
+      this.page.page = newPage // 赋值最新页码
+      this.getEmployeeList() // 重新拉去数据
+    }
+  }
 }
 </script>
 

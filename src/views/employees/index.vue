36840c54b585641bcc1b0d8f7e9b2e11a26ad95a
NFC <1972846283@qq.com>
2022-03-31 15:31:22 +0800

    引用弹出层,点击弹出

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -11,7 +11,12 @@
         <template v-slot:after>
           <el-button size="small" type="success">excel导入</el-button>
           <el-button size="small" type="danger">excel导出</el-button>
-          <el-button size="small" type="primary">新增员工</el-button>
+
+          <el-button
+            size="small"
+            type="primary"
+            @click="showDialog = true"
+          >新增员工</el-button>
         </template>
       </page-tools>
       <!-- 表格组件 -->
@@ -81,7 +86,8 @@
       </el-row>
     </div>
     <!-- 放置组件弹层 -->
-    <AddEmployee />
+    <!-- sync修饰符 是子组件 去改变父组件的数据的一个语法糖 -->
+    <AddEmployee :show-dialog.sync="showDialog" />
   </div>
 </template>
 
@@ -100,7 +106,8 @@ export default {
         size: 10,
         total: 0 // 总数
       },
-      loading: false // 显示遮罩层
+      loading: false, // 显示遮罩层
+      showDialog: false // 默认关闭弹层
     }
   },
   created() {

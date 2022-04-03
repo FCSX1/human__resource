9cbc94beeb6a580eb0bdc706d1580d9cae195ee4
NFC <1972846283@qq.com>
2022-04-03 11:22:30 +0800

    完成了读取保存岗位信息，修复了接口的bug问题，修改了代理地址

--- src/views/employees/components/job-info.vue
+++ src/views/employees/components/job-info.vue
@@ -212,6 +212,7 @@
 
 <script>
 import EmployeeEnum from '@/api/constant/employees'
+import { getEmployeeSimple, getJobDetail, updateJob } from '@/api/employees'
 
 export default {
   data() {
@@ -249,8 +250,21 @@ export default {
       }
     }
   },
+  created() {
+    this.getJobDetail()
+    this.getEmployeeSimple()
+  },
   methods: {
-    saveJob() {}
+    async getJobDetail() {
+      this.formData = await getJobDetail(this.userId)
+    },
+    async getEmployeeSimple() {
+      this.depts = await getEmployeeSimple()
+    },
+    async saveJob() {
+      await updateJob(this.formData)
+      this.$message.success('保存岗位信息成功')
+    }
   }
 }
 </script>

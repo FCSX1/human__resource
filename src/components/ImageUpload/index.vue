43376d0acc6ece9dec2614de1f403ed8c7c05f39
NFC <1972846283@qq.com>
2022-04-03 16:53:23 +0800

    上传和添加图片

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -2,13 +2,16 @@
   <div>
     <!-- 给action一个#号 就不会报错了 -->
     <!-- file-list是上传的文件列表 可以绑定到上传组件上，让上传组件来显示 -->
+    <!-- upload组件显示的是file-list -->
     <el-upload
       list-type="picture-card"
       :limit="1"
       action="#"
       :on-preview="preview"
+      :on-remove="handleRemove"
+      :on-change="changeFile"
       :file-list="fileList"
-      :class="{ disabled : fileComputed }"
+      :class="{ disabled: fileComputed }"
     >
       <i class="el-icon-plus" />
     </el-upload>
@@ -42,13 +45,25 @@ export default {
     preview(file) {
       this.imgUrl = file.url
       this.showDialog = true
+    },
+    handleRemove(file, fileList) {
+      this.fileList = this.fileList.filter(item => item.uid !== file.uid) // 将当前的删除文件排除在外
+    //   this.fileList = fileList  // 两种方式都可
+    },
+    // 这里不能用push 这个钩子会执行多次
+    changeFile(file, fileList) {
+      // file是当前的文件  fileList是当前的最新数组 this.fileList
+      this.fileList = fileList.map(item => item)
+      // this.fileList = fileList.map(item => item) 这里为什么暂时不成功？ 因为现在还没有上传 所有第二次进来的数据 一定是个空的
+      // 如果完成上传动作了 第一次进入 和第二次进去的fileList的长度应该都是1 应该都有数据
+      // 上传成功 => 数据才能进来
     }
   }
 }
 </script>
 
-<style scoped>
+<style >
 .disabled .el-upload--picture-card {
-    display: none;
+  display: none
 }
 </style>

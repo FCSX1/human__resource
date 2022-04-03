0d2d2b45b45b2455b3a8775d1f3b51626045ebe1
NFC <1972846283@qq.com>
2022-04-03 14:59:33 +0800

    新建文件上传组件，点击图片进行预览

---
+++ src/components/ImageUpload/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <!-- 给action一个#号 就不会报错了 -->
+    <!-- file-list是上传的文件列表 可以绑定到上传组件上，让上传组件来显示 -->
+    <el-upload
+      list-type="picture-card"
+      :limit="1"
+      action="#"
+      :on-preview="preview"
+      :file-list="fileList"
+    >
+      <i class="el-icon-plus" />
+    </el-upload>
+    <el-dialog :visible.sync="showDialog" title="图片预览">
+      <img
+        :src="imgUrl"
+        alt=""
+        style="width:100%"
+      >
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fileList: [{ url: 'https://img0.baidu.com/it/u=178892670,2966992691&fm=253&fmt=auto&app=138&f=JPEG?w=400&h=400' }],
+      showDialog: false,
+      imgUrl: ''
+    }
+  },
+  methods: {
+    // 点击预览事件
+    preview(file) {
+      console.log(file.url)
+      this.imgUrl = file.url
+      this.showDialog = true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>

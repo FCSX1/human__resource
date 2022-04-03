d3208eedf2028b80da4b7bf110722cf7b406aaa8
NFC <1972846283@qq.com>
2022-04-03 20:09:43 +0800

    上传成功之后处理返回数据

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -37,9 +37,10 @@ const cos = new COS({
 export default {
   data() {
     return {
-      fileList: [{ url: 'https://img0.baidu.com/it/u=178892670,2966992691&fm=253&fmt=auto&app=138&f=JPEG?w=400&h=400' }],
+      fileList: [],
       showDialog: false,
-      imgUrl: ''
+      imgUrl: '',
+      currentFileUid: null // 记录当前正在上传的uid
     }
   },
   computed: {
@@ -81,6 +82,8 @@ export default {
         this.$message.error('上传的图片大小不能大于5M')
         return false
       }
+      // 已经确定当前上传的就是当前的这个file了
+      this.currentFileUid = file.uid
       return true // 最后一定要return true
     },
     // 进行上传操作
@@ -94,9 +97,27 @@ export default {
           Key: params.file.name, // 文件名
           Body: params.file, // 要上传的文件对象
           StorageClass: 'STANDARD' // 上传的模式类型 直接默认 标准模式即可
-        }, function(err, data) {
+        }, (err, data) => {
           // data返回数据之后 应该如何处理
           console.log(err || data)
+          // data中有一个statusCode === 200 的时候说明上传成功
+          if (!err && data.statusCode === 200) {
+            // 此时说明文件上传成功 要获取成功的返回地址
+            // fileList才能显示到上传组件上 此时我们要将fileList中的数据的url地址变成 现在上传成功的地址
+            // 目前虽然上一张图片 但是请注意 我们的fileList是一个数组
+            // 需要知道当前上传成功的是那一张图片
+            this.fileList = this.fileList.map(item => {
+              // 去找谁的uid等于刚刚记录下来的id
+              if (item.uid === this.currentFileUid) {
+                // 将成功的地址赋值给原来的url属性
+                return { url: 'http://' + data.Location, upload: true }
+                // upload 为true 表示这张图片已经上传完毕 这个属性要为我们后期应用的时候做标记
+                // 保存 => 图片有大有小 => 上传速度有快有慢 => 要根据有没有upload这个标记来决定是否去保存
+              }
+              return item
+            })
+            // 将上传成功的地址 回写 到了fileList中 fileList变化 => upload组件 就会根据fileList的变化而去渲染视图
+          }
         })
       }
     }

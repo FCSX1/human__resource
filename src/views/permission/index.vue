fe90f2252c59a72c9f10528b7d14ac6da4a81535
NFC <1972846283@qq.com>
2022-04-05 16:32:01 +0800

    获取权限数据并转化树形

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -7,16 +7,29 @@
         <el-button slot="after" type="primary" size="small">添加权限</el-button>
       </page-tools>
       <!-- 表格 -->
-      <el-table border="">
-        <el-table-column align="center" label="名称" />
+      <!-- 指定id为唯一属性的标识 -->
+      <el-table border :data="list" row-key="id">
+        <el-table-column
+          label="名称"
+          prop="name"
+        />
 
-        <el-table-column align="center" label="标识" />
+        <el-table-column
+          align="center"
+          label="标识"
+          prop="code"
+        />
 
-        <el-table-column align="center" label="描述" />
+        <el-table-column
+          align="center"
+          label="描述"
+          prop="description"
+        />
 
         <el-table-column align="center" label="操作">
-          <template>
-            <el-button type="text" size="small">添加</el-button>
+          <template v-slot="{ row }">
+            <!-- 添加按钮只在 访问权的层级显示 当type==1 时才显示添加按钮 -->
+            <el-button v-if="row.type === 1" type="text" size="small">添加</el-button>
             <el-button type="text" size="small">编辑</el-button>
             <el-button type="text" size="small">删除</el-button>
           </template>
@@ -27,8 +40,23 @@
 </template>
 
 <script>
+import { getPermissionList } from '@/api/permission'
+import { tranListToTreeData } from '@/utils'
 export default {
-
+  data() {
+    return {
+      list: []
+    }
+  },
+  created() {
+    this.getPermissionList()
+  },
+  methods: {
+    async  getPermissionList() {
+      // 记那个数据转化成了 待chilren的树形结构
+      this.list = tranListToTreeData(await getPermissionList(), '0')
+    }
+  }
 }
 </script>
 

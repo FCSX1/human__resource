e96a37ee8b15f2bd0b01de8e7b215c365fdd39e0
NFC <1972846283@qq.com>
2022-04-07 14:51:08 +0800

    提交一个离职审批

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -42,10 +42,10 @@
                     <img src="@/assets/common/img.jpeg" alt="">
                     <div>
                       <p>
-                        <span class="col">朱继柳</span> 发布了
-                        第1期“传智大讲堂”互动讨论获奖名单公布
+                        <span class="col">xxx</span> 发布了
+                        xxx互动讨论获奖名单公布
                       </p>
-                      <p>2018-07-21 15:21:38</p>
+                      <p>2022-04-25 15:21:38</p>
                     </div>
                   </div>
                 </li>
@@ -54,10 +54,10 @@
                     <img src="@/assets/common/img.jpeg" alt="">
                     <div>
                       <p>
-                        <span class="col">朱继柳</span> 发布了
-                        第2期“传智大讲堂”互动讨论获奖名单公布
+                        <span class="col">xxx</span> 发布了
+                        xxx互动讨论获奖名单公布
                       </p>
-                      <p>2018-07-21 15:21:38</p>
+                      <p>2022-04-21 15:21:38</p>
                     </div>
                   </div>
                 </li>
@@ -66,10 +66,10 @@
                     <img src="@/assets/common/img.jpeg" alt="">
                     <div>
                       <p>
-                        <span class="col">朱继柳</span> 发布了
-                        第3期“传智大讲堂”互动讨论获奖名单公布
+                        <span class="col">xxx</span> 发布了
+                        xxx互动讨论获奖名单公布
                       </p>
-                      <p>2018-07-21 15:21:38</p>
+                      <p>2022-03-21 15:21:38</p>
                     </div>
                   </div>
                 </li>
@@ -85,10 +85,13 @@
             <span>流程申请</span>
           </div>
           <div class="sideNav">
-            <el-button class="sideBtn">加班离职</el-button>
+            <el-button
+              class="sideBtn"
+              @click="showDialog = true"
+            >加班离职</el-button>
             <el-button class="sideBtn">请假调休</el-button>
-            <el-button class="sideBtn">审批列表</el-button>
-            <el-button class="sideBtn">我的信息</el-button>
+            <el-button class="sideBtn" @click="$router.push('/users/approvals')">审批列表</el-button>
+            <el-button class="sideBtn" @click="$router.push('/users/info')">我的信息</el-button>
           </div>
         </el-card>
 
@@ -130,6 +133,45 @@
         </el-card>
       </el-col>
     </el-row>
+    <!-- 离职弹层 -->
+    <el-dialog :visible="showDialog" title="离职申请" @close="btnCancel">
+      <!-- 表单内容 -->
+      <el-form
+        ref="ruleForm"
+        label-width="120px"
+        :model="ruleForm"
+        :rules="rules"
+      >
+        <el-form-item label="期望离职时间" prop="exceptTime">
+          <!-- 离职时间 返回的值就是 字符串结构了 -->
+          <el-date-picker
+            v-model="ruleForm.exceptTime"
+            type="datetime"
+            value-format="yyyy-MM-dd HH:mm:ss"
+          />
+        </el-form-item>
+        <!-- 放置输入框 -->
+        <el-form-item label="离职原因">
+          <el-input
+            v-model="ruleForm.reason"
+            type="textarea"
+            :rows="3"
+            style="width:70%"
+          />
+        </el-form-item>
+      </el-form>
+      <!-- 确定取消 -->
+      <el-row slot="footer" type="flex" justify="center">
+        <el-col :span="6">
+          <el-button
+            size="small"
+            type="primary"
+            @click="btnOK"
+          >确定</el-button>
+          <el-button size="small" @click="btnCancel">取消</el-button>
+        </el-col>
+      </el-row>
+    </el-dialog>
   </div>
 </template>
 
@@ -138,17 +180,58 @@ import { mapGetters, createNamespacedHelpers } from 'vuex'
 const { mapState } = createNamespacedHelpers('user')
 import WorkCalendar from './components/work-calendar.vue'
 import Radar from './components/radar.vue'
+import { startProcess } from '@/api/approvals'
 export default {
   name: 'Dashboard',
   components: { WorkCalendar, Radar },
   data() {
     return {
-      defaultImg: require('@/assets/common/head.jpg')
+      defaultImg: require('@/assets/common/head.jpg'),
+      showDialog: false,
+      // 离职表单
+      ruleForm: {
+        // 离职时间
+        exceptTime: '',
+        // 离职原因
+        reason: '',
+        processKey: 'process_dimission', // 特定的审批
+        processName: '离职'
+      },
+      rules: {
+        exceptTime: [{ trigger: 'blur', required: true, message: '离职时间不能为空' }],
+        reason: [{ trigger: 'blur', required: true, message: '离职原因不能为空' }]
+      }
     }
   },
   computed: {
     ...mapGetters(['name', 'staffPhoto']),
     ...mapState(['userInfo'])
+  },
+  methods: {
+    btnOK() {
+      // this.$refs.ruleForm.validate(isOK => {})
+      // this.$refs.ruleForm.validate().then()
+      // await this.$refs.ruleForm.validate().then()
+      this.$refs.ruleForm.validate(async isOK => {
+        if (isOK) {
+          // 调用离职申请 流程接口
+          await startProcess({ ...this.ruleForm, userId: this.userInfo.userId, username: this.userInfo.username })
+          // 弹出提示
+          this.$message.success('离职申请提交成功')
+          this.showDialog = false
+        }
+      })
+    },
+    btnCancel() {
+      this.ruleForm = {
+        exceptTime: '',
+        reason: '',
+        processKey: 'process_dimission',
+        processName: '离职'
+      }
+      this.$refs.ruleForm.resetFields()
+      this.showDialog = false
+    }
   }
 }
 </script>

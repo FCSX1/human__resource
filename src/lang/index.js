240dd4ac18726e132507e8e40b6f118f77b2f161
NFC <1972846283@qq.com>
2022-04-07 16:50:49 +0800

    引入自定义语言包

--- src/lang/index.js
+++ src/lang/index.js
@@ -4,6 +4,10 @@ import VueI18n from 'vue-i18n'
 import Cookie from 'js-cookie' // 引入cookie工具
 import elementZH from 'element-ui/lib/locale/lang/zh-CN' // 中文包
 import elementEN from 'element-ui/lib/locale/lang/en' // 鸟语包
+
+import customZH from './zh' // 自定义语言包
+import customEN from './en' // 自定义语言包
+
 Vue.use(VueI18n) // 完成全局注册
 export default new VueI18n({
   // i18n的选项
@@ -11,10 +15,12 @@ export default new VueI18n({
   messages: {
     zh: {
       // 语言包  elementUI 语言包 + 自定义语言包
-      ...elementZH
+      ...elementZH,
+      ...customZH
     },
     en: {
-      ...elementEN
+      ...elementEN,
+      ...customEN
     }
   } // 指的是当前的语言包
 })
